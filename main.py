c074c66b1a65cec17717aa470679671ca5a57445
Afsh-0 <248272946+Afsh-0@users.noreply.github.com>
2026-04-23 06:47:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from src.extract import extract_text
+from src.preprocess import clean_text
+from src.nlp_pipeline import extract_entities
+from src.insights import generate_insights
+from src.visualization import plot_entity_frequency
+
+pdf_path = "data/report.pdf"
+
+raw_text = extract_text(pdf_path)
+cleaned_text = clean_text(raw_text)
+
+entities = extract_entities(cleaned_text)
+
+insights = generate_insights(entities)
+
+print("\nPOLICY INSIGHTS:\n")
+for i in insights:
+    print("-", i)
+
+print("\nKEY ENTITIES:\n")
+for k, v in entities.items():
+    print(k, ":", list(set(v))[:8])
+
+
+plot_entity_frequency(entities)
